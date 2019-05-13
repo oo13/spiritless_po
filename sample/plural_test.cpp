40cf95738e2f64328f4a89b107ad2c48958d20d2
OOTA, Masato <ooyooxei+gh@gmail.com>
2019-05-13 19:34:31 +0900

    spiritless_po is licensed by GPL-3+ now.

--- sample/plural_test.cpp
+++ sample/plural_test.cpp
@@ -1,3 +1,17 @@
+/* sample/plural_test.cpp
+
+Copyright © 2019 OOTA, Masato
+
+spiritless_po is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+spiritless_po is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+*/
 #include <iostream>
 #include <iterator>
 #include <string>
